d9ece66e456116c1751c990d8f5e091e5ed77843
Eli Gripenstraw <eligripenstraw@gmail.com>
2024-12-07 16:06:40 -0600

    Update get_links.py

--- get_links.py
+++ get_links.py
@@ -17,8 +17,8 @@ import time # to sleep
 
 # fill this in with your job preferences!
 PREFERENCES = {
-    "position_title": "Software Engineer",
-    "location": "San Francisco, CA"
+    "position_title": "Machine Learning Intern",
+    "location": "Chicago"
 }
 
 # helper method to give user time to log into glassdoor
